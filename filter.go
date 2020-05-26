55c7814415f588dc52c4fa7ff106e316082f4a8e
mingard <info@arthurmingard.com>
2020-05-26 10:06:01 +0000

    fix: correct type def

--- filter.go
+++ filter.go
@@ -108,7 +108,7 @@ func (o *OneOfFilter) String() string {
 }
 
 // IsOneOf returns a new instance of NumberFilter.
-func IsOneOf(f, v []string) FieldFilter {
+func IsOneOf(f string, v []string) FieldFilter {
 	return &OneOfFilter{
 		Field:    f,
 		Operator: In,
